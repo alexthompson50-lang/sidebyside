ef2fa3b99dd45c3d542b4a723b01497fb3e81103
alexthompson50-lang <alexthompson50@gmail.com>
2026-03-12 22:30:03 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import PublicCard from "./PublicCard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PublicCard />} />
+        <Route path="/edit" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
